aadc441587830af8120cd1702d682e68c974dd9b
liuyongjin <572551431@qq.com>
2020-01-15 22:10:41 +0800

    fix:修改域名

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,7 +4,7 @@ const moment =require('moment');
 const reg = /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(?::\d+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/;
 
 // export const BASE_URL='http://blog.com';
-export const BASE_URL='https://api.liuyongjin.cn';
+export const BASE_URL='http://api.liuyongjin.cn';
 
 export const isUrl = (path: string): boolean => reg.test(path);
 
